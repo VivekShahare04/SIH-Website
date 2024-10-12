95752940e0ac387a598f643582c6e5e9d143c038
VivekShahare04 <mastervivek23@gmail.com>
2024-10-12 22:19:47 +0530

    updated

--- Main/frontend/src/pages/Resources.js
+++ Main/frontend/src/pages/Resources.js
@@ -9,7 +9,7 @@ const Resources = () => {
     {
       title: 'Dataset by Dr.Sachin Jadhao',
       description: 'Train a machine learning model on a 20,000-sample dataset created by Dr.Sachin Jadhao (Chemical Engineering ,Phd.ICT Bombay & Post doct.Cornell University,USA ) to efficiently predict outcomes. The process includes data preprocessing, feature selection, and model training, enhancing accuracy through techniques like cross-validation.',
-      link: 'https://drive.google.com/file/d/1Lct5Rv71bjEpwOaY_2Tkf_cbaru2pwfl/view',
+      link: 'https://drive.google.com/file/d/1hYHTB2Ml_3umd907s7LqfgYDNCW7f08w/view?usp=sharing',
     },
     {
       title: 'Integration of IOT + Machine Learning',
